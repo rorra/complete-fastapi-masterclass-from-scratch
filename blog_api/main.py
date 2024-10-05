7bf425ad2aa0bca9cdd461427785197c37577929
Rodrigo Dominguez <rorra@rorra.com.ar>
2024-10-04 21:57:23 -0300

    Upload image

--- blog_api/main.py
+++ blog_api/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from db import models
 from db.database import engine
 from routers import post
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(post.router)
@@ -13,3 +14,5 @@ def read_root():
 
 
 models.Base.metadata.create_all(engine)
+
+app.mount("/images", StaticFiles(directory="images"), name="images")
--- blog_api/routers/post.py
+++ blog_api/routers/post.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, File, UploadFile
 from routers.schema import PostBase, PostDisplay
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_post
+import string
+import random
 
 router = APIRouter(
     prefix="/posts",
@@ -23,3 +25,19 @@ def get_all(db: Session = Depends(get_db)):
 @router.delete("/{id}")
 def delete(id: int, db: Session = Depends(get_db)):
     return db_post.delete(db, id)
+
+
+@router.post('/image')
+def upload_image(image: UploadFile = File(...)):
+    letter = string.ascii_letters
+    rand_str = ''.join(random.choice(letter) for i in range(6))
+
+    file_name = f"{image.filename.rsplit('.', 1)[0]}_{rand_str}.{image.filename.rsplit('.', 1)[1]}"
+    # file_name = f"_{rand_str}.{image.filename.rsplit('.', 1)[1]}"
+    # file_name = f"{image.filename.rsplit('.', 1)[0]}_{rand_str}."
+    path = f"images/{file_name}"
+
+    with open(path, 'w+b') as buffer:
+        buffer.write(image.file.read())
+
+    return {"file_name": path}
